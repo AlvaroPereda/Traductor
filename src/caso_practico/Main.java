efeacab14e35d8e412e677eb8db2658cbc55c6af
AlvaroPereda <121577598+AlvaroPereda@users.noreply.github.com>
2023-12-19 19:17:38 +0100

    prueba

---
+++ src/caso_practico/Main.java
@@ -0,0 +1,9 @@
+package caso_practico;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		System.out.println("Buenas");
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module caso_practico {
+}
